fa690728b7b365df69716b0a1e369d0857b6558b
Yifei Yan <yanyiphei@gmail.com>
2025-04-10 16:32:28 -0700

    added changes

--- visualizer.py
+++ visualizer.py
@@ -127,13 +127,14 @@ if r:
                             st.text(msg["msg_content"])
                         elif msg["fn_call_to_fn_output"]:
                             st.write("**Tool Calls**")
-                            for fn_call_dict in msg["fn_call_to_fn_output"]:
+                            for i, fn_call_dict in enumerate(msg["fn_call_to_fn_output"]):
                                 new_dict = fn_call_dict["function_call"].copy()
                                 new_dict.pop("input_args_json")
                                 new_dict.pop("source_provider")
                                 new_dict["output"] = fn_call_dict["value"]
                                 st.json(new_dict)
-                                st.divider()
+                                if 0 < i < len(msg["fn_call_to_fn_output"]) - 1:
+                                    st.divider()
 
     # --- Main Area: Display Chat ---
     if st.session_state.selected_task_uuid is not None:
@@ -152,9 +153,6 @@ if r:
         else:
             st.header(f"Task {st.session_state.selected_task_uuid}")
 
-        # Define the fragment that will auto-refresh
-
-        # Call the fragment function to display the chat
         display_chat_messages()
 
     else:
